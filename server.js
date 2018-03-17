ba052defc66b00a5009975b73549a3ba443b05d6
Pranshu Jethmalani <pmj642@gmail.com>
2018-03-17 12:23:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -56,8 +56,6 @@ app.post('/create-user', function (req,res) {
 app.post('/login', function (req,res) {
     var username = req.body.username;
     var password = req.body.password;
-    var salt = crypto.randomBytes(128).toString('hex');
-    var dbString = hash(password,salt);
     
     pool.query('select from "user" where username = $1', username, (err,result) => {
         if(err) {
