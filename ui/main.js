f982d456afb77440a6903b45fa41eca493b9da61
Pranshu Jethmalani <pmj642@gmail.com>
2018-03-17 13:16:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,7 +24,7 @@ var password = document.getElementById('password').value;
 console.log(username);
 console.log(password);
 request.open('POST','http://pmj642.imad.hasura-app.io/login',true);
-request.setRequestHeader = 'application/json';
+request.setRequestHeader('Content-Type','application/json');
 request.send(JSON.stringify({username: username,password: password}));
 
 };
